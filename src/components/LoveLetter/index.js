363b0bb718cb9b3e12534e113ee806520e098ac6
raireddyshyam <raireddyshyam@gmail.com>
2024-04-29 21:50:44 +0530

    first commit

---
+++ src/components/LoveLetter/index.js
@@ -0,0 +1,47 @@
+import React, { useState, useRef } from 'react';
+import './index.css';
+import audioFile from './kushii.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+      My dear Kedhasree garu🖤,<br />
+        I wanted to thank you for our conversation today.I really enjoyed talking with you for an hour.I felt like you're someone I can always be myself around.Thanks buddy (Small gratitute from my side)<br />I hope you will be my bestie🖤<br />
+        @ Shyam<br />
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
